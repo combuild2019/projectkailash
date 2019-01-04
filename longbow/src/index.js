f6db4110524987b7c7e9d05a3d14f3fa567a4eda
Siddharth Bhawalkar <siddharth.bhawalkar@sapient.com>
2019-01-04 13:21:10 -0500

    added user login and registration page

--- longbow/src/index.js
+++ longbow/src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+import { Provider } from 'react-redux';
+
+import { store } from './shared/helpers';
+import { configureFakeBackend } from './shared/helpers';
+
+import './styles/index.scss';
+
+import { App } from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// setup fake backend
+configureFakeBackend();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
+
 serviceWorker.unregister();
---
+++ longbow/src/shared/Config.js
@@ -0,0 +1,3 @@
+export const Config = () => {
+    this.apiUrl = 'http://localhost:3000'
+}
